89a28ea4eab53af3b03c32671f40b7d17b5dfe94
Areagi_Miane <154210106+Mianeeeee@users.noreply.github.com>
2025-10-14 00:44:15 +0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -2,9 +2,6 @@ import java.math.BigDecimal;
 import java.util.*;
 
 public class Customer extends Human {
-    private String tourBookings;
-    private String bookingState;
-    private String bookingDate;
     private int numberOfCustomers;
     private BigDecimal price;
     private static Map<String, Customer> CustomerList = new HashMap<>();
@@ -15,35 +12,11 @@ public class Customer extends Human {
 
     public Customer() {
         super();
-        this.tourBookings = "";
-        this.bookingState = "";
-        this.bookingDate = "";
         this.numberOfCustomers = 0;
         this.price = new BigDecimal("0.00");
     }
 
-    public Customer(String Id, String Name, String Birthday, String PhoneNumber, String Email, String tourBookings,
-            String bookingState, String bookingDate, int numberOfCustomers, BigDecimal price) {
-        super(Id, Name, Birthday, PhoneNumber, Email);
-        this.tourBookings = tourBookings;
-        this.bookingState = bookingState;
-        this.bookingDate = bookingDate;
-        this.numberOfCustomers = numberOfCustomers;
-        this.price = price;
-    }
-
     // Get
-    public String getTourBooking() {
-        return this.tourBookings;
-    }
-
-    public String getBookingState() {
-        return this.bookingState;
-    }
-
-    public String getBookingDate() {
-        return this.bookingDate;
-    }
 
     public int getNumberOfCustomers() {
         return this.numberOfCustomers;
@@ -54,17 +27,6 @@ public class Customer extends Human {
     }
 
     // Set
-    public void setTourBooking(String tourBookings) {
-        this.tourBookings = tourBookings;
-    }
-
-    public void setBookingState(String bookingState) {
-        this.bookingState = bookingState;
-    }
-
-    public void setBookingDate(String bookingDate) {
-        this.bookingDate = bookingDate;
-    }
 
     public void setNumberOfCustomers(int numberOfCustomers) {
         this.numberOfCustomers = numberOfCustomers;
