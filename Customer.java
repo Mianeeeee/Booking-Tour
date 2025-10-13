ab2d402f1822b9667aab8a4fa0d525089b9d36fc
Areagi_Miane <154210106+Mianeeeee@users.noreply.github.com>
2025-10-13 22:13:33 +0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -2,7 +2,6 @@ import java.math.BigDecimal;
 import java.util.*;
 
 public class Customer extends Human {
-
     private String tourBookings;
     private String bookingState;
     private String bookingDate;
@@ -33,40 +32,42 @@ public class Customer extends Human {
         this.price = price;
     }
 
+    // Get
     public String getTourBookings() {
         return this.tourBookings;
-    }
-
-    public void setTourBookings(String tourBookings) {
-        this.tourBookings = tourBookings;
     }
 
     public String getBookingState() {
         return this.bookingState;
     }
 
-    public void setBookingState(String bookingState) {
-        this.bookingState = bookingState;
-    }
-
     public String getBookingDate() {
         return this.bookingDate;
-    }
-
-    public void setBookingDate(String bookingDate) {
-        this.bookingDate = bookingDate;
     }
 
     public int getNumberOfCustomers() {
         return this.numberOfCustomers;
     }
 
-    public void setNumberOfCustomers(int numberOfCustomers) {
-        this.numberOfCustomers = numberOfCustomers;
-    }
-
     public BigDecimal getPrice() {
         return this.price;
+    }
+
+    // Set
+    public void setTourBookings(String tourBookings) {
+        this.tourBookings = tourBookings;
+    }
+
+    public void setBookingState(String bookingState) {
+        this.bookingState = bookingState;
+    }
+
+    public void setBookingDate(String bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public void setNumberOfCustomers(int numberOfCustomers) {
+        this.numberOfCustomers = numberOfCustomers;
     }
 
     public void setPrice(BigDecimal price) {
